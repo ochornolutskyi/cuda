625706c75dd7b2ac7a13f5979f95cd0932b07d13
ochornolutskyi <ochornolutskyi@gmail.com>
2019-11-27 10:44:39 +0200

    some changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(function() {
   //SCROLLING
   //scrolling navigation scroll
-  $("#nav").on("click", "a", function(event) {
+  $("#nav>ul>li:not(:first-child)").on("click", "a", function(event) {
     event.preventDefault();
     let id = $(this).attr("href"),
       top = $(id).offset().top;
@@ -26,6 +26,11 @@ $(function() {
     return false;
   });
 
+  //CONTACT FORM BACK BUTTON
+  $(window).ready(function() {
+    $(".back-button").show(500);
+  });
+
   //ERRORS
   let addError = function(message) {
     return $(
@@ -43,58 +48,134 @@ $(function() {
       callback();
     });
   };
-  let notActive = function(getElement) {
+  let lockButton = function(getElement) {
     if (getElement.is("a")) return getElement.css("pointer-events", "none");
     if (getElement.is("button")) return getElement.attr("disabled", true);
   };
-  let isActive = function(getElement) {
+  let unlockButton = function(getElement) {
     if (getElement.is("a")) return getElement.css("pointer-events", "auto");
     if (getElement.is("button")) return getElement.removeAttr("disabled");
   };
+  //errors block
   $("#blogLink").click(function(event) {
     event.preventDefault();
     message = "we have not blog page :(";
-    notActive($(this));
+    lockButton($(this));
     $(addError(message));
     hideError(() => {
-      isActive($(this));
+      unlockButton($(this));
     });
   });
   $(".gallery-button").click(function(event) {
     event.preventDefault();
     message = "no more projects";
-    notActive($(this));
+    lockButton($(this));
     $(addError(message));
     hideError(() => {
-      isActive($(this));
+      unlockButton($(this));
     });
   });
-
-  //GALLERY CHANGES
+  $("#dribble").click(function(event) {
+    event.preventDefault();
+    message = "dribble account will add soon";
+    lockButton($(this));
+    $(addError(message));
+    hideError(() => {
+      unlockButton($(this));
+    });
+  });
+  $("#behance").click(function(event) {
+    event.preventDefault();
+    message = "behance account will add soon";
+    lockButton($(this));
+    addError(message);
+    hideError(() => {
+      unlockButton($(this));
+    });
+  });
+  $("#google").click(function(event) {
+    event.preventDefault();
+    message = "googlePlus account will add soon";
+    lockButton($(this));
+    addError(message);
+    hideError(() => {
+      unlockButton($(this));
+    });
+  });
+  $("#twitter").click(function(event) {
+    event.preventDefault();
+    message = "twitter account will add soon";
+    lockButton($(this));
+    addError(message);
+    hideError(() => {
+      unlockButton($(this));
+    });
+  });
+  //GALLERY CHANGE SLIDES
   $(".portfolio-content__menu-button").click(function() {
-    //gallery menu button changes on active
+    //change button activity
     $(".portfolio-content__menu-button").removeClass("active");
     $(this).addClass("active");
-    //current gallery content
-    let id = $(this).attr("id");
+    //change current gallery content
+    let idButton = $(this).attr("id");
     $(".gallery-item").css({ display: "none" });
-    let elementName = "";
-    switch (id) {
+    let classElement = "";
+    switch (idButton) {
       case "buttonAll":
-        elementName = ".gallery-item";
+        classElement = ".gallery-item";
         break;
       case "buttonWeb":
-        elementName = ".gallery-item__web";
+        classElement = ".gallery-item__web";
         break;
       case "buttonApps":
-        elementName = ".gallery-item__apps";
+        classElement = ".gallery-item__apps";
         break;
       case "buttonIcons":
-        elementName = ".gallery-item__icons";
+        classElement = ".gallery-item__icons";
         break;
     }
-    return $(elementName)
+    return $(classElement)
       .fadeIn(500)
       .css({ display: "block" });
+  });
+
+  //SLICK-SLIDER CONFIG
+  $(".about-content__slider").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 2,
+    rows: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          rows: 4,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          rows: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          rows: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true
+        }
+      }
+    ]
   });
 });
